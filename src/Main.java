069a9d5600866f9b55d93d4fee44978e3fd0b8ec
marioskaraiskos <marioskaraiskosbusiness@gmail.com>
2024-08-06 16:30:05 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EventListener;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -20,9 +21,8 @@ public class Main {
         add.setBounds(100, 70, 100, 30); // x, y, width, height
         panel.add(add);
 
-        JTextField input = new JTextField(10);
-        input.setBounds(1000, 150, 400, 40);
-        panel.add(input);
+
+        //operations//
 
         JButton minus = new JButton("-");
         minus.setBounds(100, 100, 100, 30);
@@ -33,31 +33,160 @@ public class Main {
         JButton multiply = new JButton("*");
         multiply.setBounds(100, 160, 100, 30);
         panel.add(multiply);
-        JButton eq = new JButton("=");
-        eq.setBounds(100, 200, 300, 60);
-        panel.add(eq);
-        int x = 200;
-        int y = 70;
-        int i = 0;
-        for ( i = 0; i<10; i++) {
-            JButton b = new JButton("" + i);
-            b.setBounds(x, y, 100, 30);
-            x = x + 100;
-            if (i == 0) {
-                y = y + 30;
-                x = x - 100;
-            }
-            else if ( i == 3 ) {
-                y = y + 30;
-                x = x - 300;
-            }
-            else if ( i == 6 ) {
-                y = y + 30;
-                x = x - 300;
-            }
 
-            panel.add(b);
-        }
+
+
+        //numbers//
+
+        JButton one = new JButton("1");
+        one.setBounds(200, 70, 100, 30);
+        panel.add(one);
+        JButton two = new JButton("2");
+        two.setBounds(300, 70, 100, 30);
+        panel.add(two);
+        JButton three = new JButton("3");
+        three.setBounds(400, 70, 100, 30);
+        panel.add(three);
+        JButton four = new JButton("4");
+        four.setBounds(200, 100, 100, 30);
+        panel.add(four);
+        JButton five = new JButton("5");
+        five.setBounds(300, 100, 100, 30);
+        panel.add(five);
+        JButton six = new JButton("6");
+        six.setBounds(400, 100, 100, 30);
+        panel.add(six);
+        JButton seven = new JButton("7");
+        seven.setBounds(200, 130, 100, 30);
+        panel.add(seven);
+        JButton eight = new JButton("8");
+        eight.setBounds(300, 130, 100, 30);
+        panel.add(eight);
+        JButton nine = new JButton("9");
+        nine.setBounds(400, 130, 100, 30);
+        panel.add(nine);
+        JButton zero = new JButton("0");
+        zero.setBounds(300, 160, 100, 30);
+        panel.add(zero);
+        JButton clear = new JButton("clear");
+        clear.setBounds(200, 160, 100, 30);
+        panel.add(clear);
+        JButton equals = new JButton("=");
+        equals.setBounds(400, 160, 100, 30);
+        panel.add(equals);
+
+        //textField//
+
+        JTextField input = new JTextField(10);
+        input.setBounds(100, 190, 400, 40);
+        panel.add(input);
+
+        //adding action event listener //
+        one.addActionListener(e -> {
+            // Set the text of the JTextField to "5" when the button is pressed
+            input.setText("1");
+        });
+        one.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("1");
+            }
+        });
+        two.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("2");
+            }
+        });
+        three.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("3");
+            }
+        });
+        four.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("4");
+            }
+        });
+        five.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("5");
+            }
+        });
+        six.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("6");
+            }
+        });
+        seven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("7");
+            }
+        });
+        eight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("8");
+            }
+        });
+        nine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("9");
+            }
+        });
+        zero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("0");
+            }
+        });
+        equals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("result");
+            }
+        });
+        minus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("-");
+            }
+        });
+        multiply.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("*");
+            }
+        });
+        divide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("/");
+            }
+        });
+        clear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("");
+            }
+        });
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                input.setText("+");
+            }
+        });
+
+        //result//
+        Number result;
+         result = 0.0f;
+
 
 
 
@@ -65,4 +194,10 @@ public class Main {
         frame.setVisible(true);
         }
 
-}
+
+
+
+
+        }
+
+
